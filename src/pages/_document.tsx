acc131cff6c912e5677282a82a38d848556a833d
benitezhiago <hiagosantanabenitez@gmail.com>
2022-09-26 10:19:49 -0300

    add: Description

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -43,7 +43,11 @@ export default class MyDocument extends Document {
   render(): JSX.Element {
     return (
       <Html>
-        <title>Dra Beatriz S Benitez</title>
+        <title>Dra Beatriz | Dentista em Ji-Paraná</title>
+        <meta
+          name="description"
+          content="Dra. Beatriz S Benitez | Clínica-Geral • Facetas • Limpeza • Clareamento • Restaurações • Cirurgias • Odontopediatria"
+        />
 
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
@@ -54,6 +58,48 @@ export default class MyDocument extends Document {
           />
 
           <link rel="shortcut icon" href="./images/logo.svg" type="image/svg" />
+
+          <meta itemProp="name" content="Dra Beatriz | Dentista em Ji-Paraná" />
+          <meta
+            itemProp="description"
+            content="Dra. Beatriz S Benitez | Clínica-Geral • Facetas • Limpeza • Clareamento • Restaurações • Cirurgias • Odontopediatria
+"
+          />
+          <meta
+            itemProp="image"
+            content="https://scontent.fpbq1-1.fna.fbcdn.net/v/t39.30808-6/299192897_5331178360335883_5245899706644090382_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=J_eHKE2ZHpAAX_VqsYN&_nc_oc=AQlnEwOtb7Oyw3Q7a1EZ-S3ScXdvcYoLaZPiLmn-vw3bBxmw_xlJLXShDFo9ppWFiBE&_nc_ht=scontent.fpbq1-1.fna&oh=00_AT8pF6eeZDYiWqUMm9H23x1tR_F_pojR9nzP3_PrKnf4eA&oe=63363F23"
+          />
+
+          <meta property="og:url" content="https://www.dentistaemjipa.com" />
+          <meta property="og:type" content="website" />
+          <meta
+            property="og:title"
+            content="Dra Beatriz | Dentista em Ji-Paraná"
+          />
+          <meta
+            property="og:description"
+            content="Dra. Beatriz S Benitez | Clínica-Geral • Facetas • Limpeza • Clareamento • Restaurações • Cirurgias • Odontopediatria
+"
+          />
+          <meta
+            property="og:image"
+            content="https://scontent.fpbq1-1.fna.fbcdn.net/v/t39.30808-6/299192897_5331178360335883_5245899706644090382_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=J_eHKE2ZHpAAX_VqsYN&_nc_oc=AQlnEwOtb7Oyw3Q7a1EZ-S3ScXdvcYoLaZPiLmn-vw3bBxmw_xlJLXShDFo9ppWFiBE&_nc_ht=scontent.fpbq1-1.fna&oh=00_AT8pF6eeZDYiWqUMm9H23x1tR_F_pojR9nzP3_PrKnf4eA&oe=63363F23"
+          />
+
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta
+            name="twitter:title"
+            content="Dra Beatriz | Dentista em Ji-Paraná"
+          />
+          <meta
+            name="twitter:description"
+            content="Dra. Beatriz S Benitez | Clínica-Geral • Facetas • Limpeza • Clareamento • Restaurações • Cirurgias • Odontopediatria
+"
+          />
+          <meta
+            name="twitter:image"
+            content="https://scontent.fpbq1-1.fna.fbcdn.net/v/t39.30808-6/299192897_5331178360335883_5245899706644090382_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=J_eHKE2ZHpAAX_VqsYN&_nc_oc=AQlnEwOtb7Oyw3Q7a1EZ-S3ScXdvcYoLaZPiLmn-vw3bBxmw_xlJLXShDFo9ppWFiBE&_nc_ht=scontent.fpbq1-1.fna&oh=00_AT8pF6eeZDYiWqUMm9H23x1tR_F_pojR9nzP3_PrKnf4eA&oe=63363F23"
+          />
         </Head>
         <body>
           <Main />
