c25602689e02be0b1ce62070b0d5f7f3c76ada47
benitezhiago <hiagosantanabenitez@gmail.com>
2023-04-27 19:31:32 -0300

    add: new link

--- src/layouts/Home/index.tsx
+++ src/layouts/Home/index.tsx
@@ -5,7 +5,7 @@ function HomeLayout() {
   return (
     <S.Container>
       <img src="./images/odonto.png" alt="Logo dra beatriz" />
-      <a href="https://wa.me/5569984391965?text=VAGA+08+%7C+Ol%C3%A1+Dra.+Beatriz%2C+gostaria+de+marcar+meu+hor%C3%A1rio+para+fazer+meu+clareamento%21">
+      <a href="https://wa.me/5569984391965?text=Ol%C3%A1+Dra.+Beatriz%2C+gostaria+de+marcar+um+hor%C3%A1rio+para+atender+meu+filho%28a%29%21">
         <button>
           <img src="./images/clique.svg" alt="Icone Button Cliques" />
           AGENDE AGORA
